5e29dd5381cc4de75230bec726bcc89db3d89a5e
liuyilong720 <980743424@qq.com>
2019-12-24 22:48:56 +0800

    实现导航栏

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav_bar">
+    <div class="nav_bar_left"><slot name="left"></slot></div>
+    <div class="nav_bar_center"><slot name="center"></slot></div>
+    <div class="nav_bar_right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
+
+<style>
+  .nav_bar {
+    height: 44px;
+    line-height: 44px;
+    box-shadow: 0 1px 1px rgba(100, 100, 100, .1);
+    display: flex;
+  }
+  .nav_bar_left,.nav_bar_right {
+    width: 60px;
+  }
+  .nav_bar_center {
+    flex: 1;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="nav_bar_home">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <h2>Home</h2>
   </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style>
-
+  .nav_bar_home {
+    background-color: var(--color-tint);
+    text-align: center;
+    color: #fff;
+  }
 </style>
